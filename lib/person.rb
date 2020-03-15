d111e6d32e738f3a2bdcd017faaa62b62b59989f
Jack Madden <jck13mad@gmail.com>
2020-03-15 15:37:59 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,15 +1,15 @@
 class Person
   
-  def name=(name)
-    @name = name
+  def name=(person_name)
+    @name = person_name
   end
   
   def name
     @name
   end 
   
-  def job=(job)
-    @job = job
+  def job=(person_job)
+    @job = person_job
   end
   
   def job
